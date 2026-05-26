4ea9652dba6b2dabcad2a6fdd73b2a3e880085ea
AdityaHoode <adityahoode3@gmail.com>
2026-05-26 12:13:36 +0530

    1779777801

--- NC/Two Pointers/3Sum.py
+++ NC/Two Pointers/3Sum.py
@@ -23,3 +23,33 @@ class Solution:
                         k-=1
             i+=1    
         return res
+    
+# R1
+# Optimization - Added a break when the ith value becomes positive because then the sum will never be zero as it is a sorted array
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        snums=sorted(nums)
+        res=[]
+        i=0
+        while i<len(snums)-2:
+            if snums[i]>0:
+                break
+            if i>0 and snums[i-1]==snums[i]:
+                i+=1
+                continue
+            j,k=i+1,len(snums)-1
+            while j<k:
+                if snums[i]+snums[j]+snums[k]>0:
+                    k-=1
+                elif snums[i]+snums[j]+snums[k]<0:
+                    j+=1
+                else:
+                    res.append([snums[i],snums[j],snums[k]])
+                    j+=1
+                    k-=1
+                    while j<k and snums[j-1]==snums[j]:
+                        j+=1
+                    while k>j and snums[k+1]==snums[k]:
+                        k-=1
+            i+=1
+        return res
