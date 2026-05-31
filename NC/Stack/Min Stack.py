f70fdded2afa53a3ab1a3db66f3176741627b812
AdityaHoode <adityahoode3@gmail.com>
2026-05-31 19:02:09 +0530

    1780234310

--- NC/Stack/Min Stack.py
+++ NC/Stack/Min Stack.py
@@ -19,3 +19,32 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min_stk[-1]
+
+# R1
+class MinStack:
+
+    def __init__(self):
+        self.stk=[]
+        self.min_stk=[]
+        self.min_val=0
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        if not self.min_stk:
+            self.min_val=val
+            self.min_stk.append(val)
+        else:
+            if self.min_val>=val:
+                self.min_val=val
+                self.min_stk.append(val)
+
+    def pop(self) -> None:
+        pop_val=self.stk.pop()
+        if pop_val==self.min_stk[-1]:
+            self.min_stk.pop()
+
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        return self.min_stk[-1]
