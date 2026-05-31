d4b863f28cea90ece6edcdfa010eae157e45c279
AdityaHoode <adityahoode3@gmail.com>
2026-05-31 12:34:33 +0530

    1780211057

--- NC/Two Pointers/Trapping Rain Water.py
+++ NC/Two Pointers/Trapping Rain Water.py
@@ -65,3 +65,49 @@ class Solution:
                     res+=max(cwidth*cheight,0)
             s.append((i,h))
         return res 
+
+# R1 - Stack
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stk=[]
+        area=0
+        for i,h in enumerate(height):
+            while stk and stk[-1][1]<h:
+                bottom=stk.pop()
+                if len(stk)!=0:
+                    width=i-stk[-1][0]-1
+                    height=min(stk[-1][1], h)-bottom[1]
+                    area+=height*width
+            stk.append((i,h))    
+        return area                                           
+    
+# R1 - Prefix & Suffix Arrays
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res=[0]
+        prefix_max=0
+        for i in range(1,len(height)):
+            prefix_max=max(prefix_max,height[i-1])
+            res.append(prefix_max)
+        suffix_max=0
+        for j in range(len(height)-2,-1,-1):
+            suffix_max=max(suffix_max,height[j+1])
+            res[j+1]=max(min(res[j+1],suffix_max)-height[j+1],0)
+        return sum(res)
+    
+# R1 - Two Pointer
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        i,j=0,len(height)-1
+        lmax,rmax=height[i],height[j]
+        area=0
+        while i<j:
+            if lmax<=rmax:
+                i+=1
+                area+=max(0, lmax-height[i])
+                lmax=max(lmax,height[i])
+            else:
+                j-=1            
+                area+=max(0, rmax-height[j])
+                rmax=max(rmax,height[j])
+        return area
