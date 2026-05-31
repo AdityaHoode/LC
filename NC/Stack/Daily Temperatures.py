82a447a2175564d2d3b609ddfdbc5837347b7673
AdityaHoode <adityahoode3@gmail.com>
2026-05-31 23:10:20 +0530

    1780249208

--- NC/Stack/Daily Temperatures.py
+++ NC/Stack/Daily Temperatures.py
@@ -8,3 +8,15 @@ class Solution:
                 res[lower_temp_index]=i-lower_temp_index
             stk.append((i,t))
         return res
+
+# R1
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stk=[]
+        res=[0]*len(temperatures)
+        for i,t in enumerate(temperatures):
+            while stk and t>temperatures[stk[-1]]:
+                prev_day=stk.pop()
+                res[prev_day]=i-prev_day
+            stk.append(i)
+        return res
--- NC/Stack/Evaluate Reverse Polish Notation.py
+++ NC/Stack/Evaluate Reverse Polish Notation.py
@@ -17,3 +17,24 @@ class Solution:
                 else:
                     stk.append(int(op1/op2))
         return int(stk[-1])
+    
+# R1
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk=[]
+        for op in tokens:
+            if op in '+-*/':
+                operator=op
+                operand2=int(stk.pop())
+                operand1=int(stk.pop())
+                if operator=='+':
+                    stk.append(operand1+operand2)
+                elif operator=='-':
+                    stk.append(operand1-operand2)
+                elif operator=='*':
+                    stk.append(operand1*operand2)
+                else:
+                    stk.append(int(operand1/operand2))
+            else:
+                stk.append(op)
+        return int(stk[-1])
