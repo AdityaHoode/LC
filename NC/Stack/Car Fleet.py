0520898e821c0dd8e34a557101b68edb82f0f561
AdityaHoode <adityahoode3@gmail.com>
2026-06-01 22:47:10 +0530

    1780334218

--- NC/Stack/Car Fleet.py
+++ NC/Stack/Car Fleet.py
@@ -23,3 +23,32 @@ class Solution:
             else:
                 stk.append(time)
         return len(stk)
+
+# R1
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stk=[]
+        res=0
+        ps_list=sorted([(p,s) for p,s in zip(position,speed)], reverse=True)
+        for p,s in ps_list:
+            t=(target-p)/s
+            if stk and stk[-1]<t:
+                res+=1
+                stk=[]
+            elif stk and stk[-1]>t:
+                continue
+            stk.append(t)
+
+        return res+1 if stk else res
+    
+# R1 - Optimized
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        res=0
+        fleet_time=0
+        for p,s in sorted(zip(position, speed), reverse=True):
+            t=(target-p)/s
+            if t>fleet_time:
+                res+=1
+                fleet_time=t
+        return res
