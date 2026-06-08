ebd1d1bd6131864074f914e2e57fe83c6bc4cd92
AdityaHoode <adityahoode3@gmail.com>
2026-06-08 19:28:47 +0530

    1780927120

--- NC/Binary Search/Find Minimum in Rotated Sorted Array.py
+++ NC/Binary Search/Find Minimum in Rotated Sorted Array.py
@@ -14,3 +14,15 @@ class Solution:
             else:
                 r=m-1
         return res
+    
+# Easily Understandable
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l,r=0,len(nums)-1
+        while l<r:
+            m=(l+r)//2
+            if nums[m]>nums[r]:
+                l=m+1
+            else:
+                r=m
+        return nums[r]
